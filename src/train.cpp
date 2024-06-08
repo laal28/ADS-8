3a1a3764f7693c0e98a31192a2d3dcdb363a543a
laal28 <129623228+laal28@users.noreply.github.com>
2024-06-08 14:23:57 +0300

    bebra

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,49 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+void Train::addCage(bool light) {
+    Cage* cage = new Cage{light, nullptr, nullptr};
+  
+    if (!first)
+    {
+        first = cage;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Cage *last = first->prev;
+        last->next = cage;
+        cage->prev = last;
+        cage->next = first;
+        first->prev = cage;
+    }
+    countOp = 0;
+}
+
+int Train::getLength() {
+    Cage *currentVan = first;
+    currentVan->light = !currentVan->light;
+      
+    for ( int res = 0, int vanCount = 0; ; )
+    {
+        currentVan = currentVan->next;
+        countOp++;
+        vanCount++;
+        
+        for ( ; !currentVan->light; urrentVan = currentVan->next, countOp++, vanCount+ );
+      
+        currentVan->light = false;
+        res = vanCount;
+      
+        for ( ; vanCount > 0; countOp++, vanCount--, currentVan = currentVan->prev );
+      
+        if (!currentVan->light) {
+            return res;
+        }
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
